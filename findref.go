4b0ca14c472515cf5fe7e0e4ac7a7f5a2b2503b5
Benjamin Porter <FreedomBen@users.noreply.github.com>
2017-08-31 13:17:49 -0600

    Initial Commit

---
+++ findref.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func listFiles() {
+
+}
+
+func printMatches(filename String, regexp Regexp) {
+
+}
+
+func main() {
+    fmt.Printf("Hello, world.\n")
+
+    listFiles();
+    printMatches();
+}
