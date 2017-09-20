293e2a35a0069901ddd51c4d957059daf0669c5a
Benjamin Porter <FreedomBen@users.noreply.github.com>
2017-09-19 21:29:24 -0600

    Playing around with some implementation

--- findref.go
+++ findref.go
@@ -1,18 +1,52 @@
 package main
 
+import "flag"
 import "fmt"
+import (
+    "regexp"
+    "os"
+)
 
-func listFiles() {
-
+func listAllFiles() {
+    fmt.Println("Listing all files")
 }
 
-func printMatches(filename String, regexp Regexp) {
+func printMatches(filename string, regexp *regexp.Regexp) {
+    fmt.Println("Printing matches")
+}
 
+func regex_start_filenames(args ...string) string {
+    return "world"
+}
+
+func printUsage() {
+    fmt.Println("Usage")
+}
+
+func usageAndExit() {
+    printUsage()
+    os.Exit(1)
 }
 
 func main() {
-    fmt.Printf("Hello, world.\n")
+    matchCasePtr := flag.Bool("match-case", false, "Match regex case (if unset smart-case is used)")
 
-    listFiles();
-    printMatches();
+    flag.Parse()
+
+    fmt.Println("matchCase: ", *matchCasePtr)
+    fmt.Println("tail: ", flag.Args())
+
+    if len(flag.Args()) < 1 {
+        fmt.Errorf("Must specify regex to match against files")
+        usageAndExit()
+    } else if len(flag.Args()) > 3 {
+        fmt.Errorf("Too many args")
+        usageAndExit()
+    }
+
+    //regex := flag.Args()
+    listAllFiles();
+
+    fmt.Println("args: ", os.Args)
+    //printMatches();
 }
