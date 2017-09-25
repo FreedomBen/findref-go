2e931fd0cb1f9d48f0e01e48d9a31b4de674e71e
Benjamin Porter <FreedomBen@users.noreply.github.com>
2017-09-24 18:15:19 -0600

    Closer to initial implementation

--- findref.go
+++ findref.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	//"errors"
-	//"net"
+	"bufio"
+	"strconv"
 )
 
 const LightRed = "\033[1;31m"
@@ -15,12 +15,10 @@ const Green = "\033[0;32m"
 const Purple = "\033[0;35m"
 const Restore = "\033[0m"
 
+const Debug = false
+
 var matchRegex *regexp.Regexp = nil
 var fileFilter *regexp.Regexp = regexp.MustCompile(".*")
-
-func printMatches(filename string, regexp *regexp.Regexp) {
-	fmt.Println("Printing matches")
-}
 
 func printUsage() {
 	fmt.Println("Usage")
@@ -31,8 +29,15 @@ func usageAndExit() {
 	os.Exit(1)
 }
 
-func printMatch(path string, lineNumber string, line string) {
-	fmt.Println(Purple + path + Restore + Green + ":" + lineNumber + ":" + Restore + LightRed + line + Restore)
+func debug(message string) {
+    if Debug {
+        fmt.Println(Green + message + Restore)
+	}
+}
+
+func printMatch(path string, lineNumber int, line []byte, match []int) {
+	// TODO:  Use match for coloring between match[0] and match[1]
+	fmt.Println(Purple + path + Restore + Green + ":" + strconv.Itoa(lineNumber) + ":" + Restore + string(line[:match[0]]) + LightRed + string(line[match[0]:match[1]]) + Restore + string(line[match[1]:]))
 }
 
 func passesFileFilter(path string) bool {
@@ -41,16 +46,46 @@ func passesFileFilter(path string) bool {
 
 func processFile(path string, info os.FileInfo, err error) error {
 	if passesFileFilter(path) {
-		// TODO: read file and match against matchRegex
-		// if lineMatches {
-		//     printMatch(path, lineNumber, line)
-		// }
-		fmt.Println(LightRed + "Processing " + Restore + Green + "file: " + Restore + Purple + path)
+		debug(LightRed + "Processing " + Restore + Green + "file: " + Restore + Purple + path)
+
+		file, err := os.Open(path)
+		// TODO: Handle err
+		if err != nil {
+			//log.Fatal(err)
+			//debug("Error 1: ", err)
+			return nil
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		var lineNumber int = 0
+		for scanner.Scan() {
+			line := scanner.Bytes()
+			for _, el := range line {
+			    if el == 0 {
+			    	// This is a binary file.  Skip it!
+			    	debug("Not processing binary file: " + path)
+			        return nil
+				}
+			}
+			lineNumber++
+			matchIndex := matchRegex.FindIndex(line)
+		    if matchIndex != nil {
+		        // we have a match! loc == nil means no match so just ignore that case
+		        printMatch(path, lineNumber, line, matchIndex)
+			}
+		}
+
+		// TODO: Handle err
+		if err := scanner.Err(); err != nil {
+			//debug("Error 2: ", err)
+			return nil
+		}
 	} else {
-		fmt.Println("Ignoring file cause it doesn't match filter: ", path)
+		debug("Ignoring file cause it doesn't match filter: " + path)
 	}
+	// TODO:  Return error if relevant
 	return nil
-	//return errors.New(nil)
 }
 
 func getMatchRegex(matchCase bool, usersRegex string) *regexp.Regexp {
@@ -66,6 +101,10 @@ func main() {
 	matchCasePtr := flag.Bool("match-case", false, "Match regex case (if unset smart-case is used)")
 
 	flag.Parse()
+
+	// add --ignore-case
+	// add mc for alias match-case
+	// add ic for alias ignore-case
 
 	fmt.Println("matchCase: ", *matchCasePtr)
 	fmt.Println("tail: ", flag.Args())
