17ece50e51287fbca4a9b526dc7c5814bef99f69
Benjamin Porter <FreedomBen@users.noreply.github.com>
2017-09-24 23:00:55 -0600

    Add colors to build scripts

--- build.rb
+++ build.rb
@@ -5,11 +5,34 @@ require 'fileutils'
 FR_VERSION = '0.0.1'.freeze
 GO_VERSION = '1.9-alpine'.freeze
 
+# See: https://stackoverflow.com/a/30068222/2062384 for list of valid targets
 OSES_ARCHES = {
   'linux' => %w(amd64 386 arm arm64),
   'windows' => %w(amd64 386),
   'darwin' => %w(amd64)
 }.freeze
+
+RED = "\033[0;31m".freeze
+BLUE = "\033[0;34m".freeze
+CYAN = "\033[0;36m".freeze
+GREEN = "\033[0;32m".freeze
+BROWN = "\033[0;33m".freeze
+BLACK = "\033[0;30m".freeze
+WHITE = "\033[1;37m".freeze
+PURPLE = "\033[0;35m".freeze
+YELLOW = "\033[1;33m".freeze
+RESTORE = "\033[0m".freeze
+LIGHT_RED = "\033[1;31m".freeze
+DARK_GRAY = "\033[1;30m".freeze
+LIGHT_GRAY = "\033[0;37m".freeze
+LIGHT_BLUE = "\033[1;34m".freeze
+LIGHT_CYAN = "\033[1;36m".freeze
+LIGHT_GREEN = "\033[1;32m".freeze
+LIGHT_PURPLE = "\033[1;35m".freeze
+
+def cyan(message)
+  puts "#{CYAN}#{message}#{RESTORE}"
+end
 
 def docker_run(os, arch)
   <<-EOS.gsub(/\s+/, ' ').gsub(/[\s\t]*\n/, ' ').strip
@@ -26,15 +49,15 @@ end
 def main
   OSES_ARCHES.each do |os, arches|
     arches.each do |arch|
-      dest_dir = "release/#{FR_VERSION}/#{os}/#{arch}"
-      puts "Building findref v#{FR_VERSION} for #{os} #{arch}..."
-      puts "Running: #{docker_run(os, arch)}"
+      dest_dir = "releases/#{FR_VERSION}/#{os}/#{arch}"
+      cyan "Building findref v#{FR_VERSION} for #{os} #{arch}..."
       system(docker_run(os, arch))
       FileUtils.mkdir_p(dest_dir)
       fr = os == 'windows' ? 'findref.exe' : 'findref'
       FileUtils.mv(fr, "#{dest_dir}/")
     end
   end
+  cyan 'Done!'
 end
 
 main
